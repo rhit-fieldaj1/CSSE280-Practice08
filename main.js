594d18874ff8856e3c205cc4c8ad85462f02b4e3
Adam Field <98504874+rhit-fieldaj1@users.noreply.github.com>
2023-09-29 23:27:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+// Q1
+function multiply(x, y) {
+    return x * y;
+}
+
+// Q2
+function isEven(num){
+    return num % 2 === 0;
+}
+
+// Q3
+const difference = function subtract(a, b) {
+    return (a - b);
+};
+
+
+// Q4
+const greeting = function(name) {
+    return `Hello, ${name}!`;
+};
+
+// Q5
+function calculate(x, y) {
+    const squareSum = function(a, b) {
+        return (a + b) ** 2;
+    }
+    
+    return squareSum(x, y);
+}
+
+
+// Q6
+function average(num1, num2, num3) {
+    const ave = function(a, b, c) {
+        return (a + b + c) / 3;
+    }
+
+    return ave(num1, num2, num3);
+}
+
+// Q7
+multiply = (a, b) => a * b;
+
+// Q8
+greet = (name) => `Hello, ${name}!`;
+
+// Q9
+square = (x) => x * x;
+
+// Q10
+isEven = (num) => num % 2 === 0;
+
+// Q11
+findMax = (numbers) => Math.max(numbers);
+
+// Q12
+addTwoNumbers = (a, b) => a + b;
